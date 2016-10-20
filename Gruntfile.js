7996bf659ce13e417b62ccd06a7dd70f9b10b356
Grzegorz Zapała <zapala.grzegorz@gmail.com>
2016-10-21 01:11:48 +0200

    Emergency commit

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        watch: {
+            scripts: {
+                files: ['sass/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                },
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'sass/sassStyle.scss'
+                }
+            }
+        },
+        // for training purpose only, no images included
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        }
+    });
+    // Load the plugins tasks 
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'imagemin']);
+};
